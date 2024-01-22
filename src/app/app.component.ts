1ae567e8067bb806cfc3c6a6e9d89af40fe81d07
Bosko Kalanovic <kalanovicbo@gmail.com>
2024-01-22 16:12:51 +0100

    crud-app inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatIconModule, MatToolbarModule, MatButtonModule, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'crud-app';
+
+
+  constructor(private _dialog: MatDialog) {}
+
+  openAddEditEmpForm() {
+    this._dialog.open(EmpAddEditComponent);
+  }
 }
---
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+
+
+@Component({
+  selector: 'app-emp-add-edit',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule
+  ],
+  templateUrl: './emp-add-edit.component.html',
+  styleUrl: './emp-add-edit.component.scss'
+})
+export class EmpAddEditComponent {
+
+}
